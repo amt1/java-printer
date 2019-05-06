8fb05636578e5f4d7982ac9dfe7180d8d9355ed7
Annabel Treshansky <annabelt@Annabels-MacBook-Air.local>
2019-05-06 15:47:38 +0100

    initialises java printer with no paper

---
+++ src/main/java/Printer.java
@@ -0,0 +1,12 @@
+public class Printer {
+
+    private short paperLeft;
+
+    public Printer() {
+        this.paperLeft = 0;
+    }
+
+    public short getPaperLeft() {
+        return paperLeft;
+    }
+}
